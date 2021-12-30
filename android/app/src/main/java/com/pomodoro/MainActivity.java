ca1cf76465b623ae2d43b46fb6222c8f5a0ada72
xiaosong <kunkkaco@gmail.com>
2021-12-30 18:32:29 +0800

    feat: app 全屏 :sparkles:

--- android/app/src/main/java/com/pomodoro/MainActivity.java
+++ android/app/src/main/java/com/pomodoro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pomodoro;
 
 import com.facebook.react.ReactActivity;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,17 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "pomodoro";
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      View decorView = getWindow().getDecorView();
+      int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+      decorView.setSystemUiVisibility(uiOptions);
+    }
   }
 }
